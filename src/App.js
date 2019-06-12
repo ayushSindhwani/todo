37b1e5c76e05da23b5802fe081291d91aa1c9921
Ayush <ayush.sindhwani@impactguru.com>
2019-06-12 09:59:53 +0530

    Finish todo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import React from "react";
 
-function App() {
-  return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
-  );
+import "bootstrap/dist/css/bootstrap.min.css";
+
+class App extends React.Component {
+  state = {
+    todoList: [],
+    todo: ""
+  };
+  addTodo = this.addTodo.bind(this);
+  handleInput = event => {
+    this.setState({ todo: event.target.value });
+  };
+  addTodo() {
+    this.setState({
+      todoList: [...this.state.todoList, this.state.todo],
+      todo: ""
+    });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Hello {this.state.todo}!</h1>
+        <input
+          type="text"
+          value={this.state.todo}
+          onChange={this.handleInput}
+        />
+        <button onClick={this.addTodo}>Add</button>
+        <ul>
+          {this.state.todoList.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
