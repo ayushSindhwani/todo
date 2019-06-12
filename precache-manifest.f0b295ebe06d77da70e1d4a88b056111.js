7e41391abe94664e06536b4a6c8abd15dc119377
Ayush <ayush.sindhwani@impactguru.com>
2019-06-12 10:24:52 +0530

    Updates

---
+++ precache-manifest.f0b295ebe06d77da70e1d4a88b056111.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd2c70ea9f0cb57b24a6bd6b98a35822",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "66ad0a45e32894c25983",
+    "url": "/todo/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "66ad0a45e32894c25983",
+    "url": "/todo/static/js/2.d2599058.chunk.js"
+  },
+  {
+    "revision": "e833ab4aba8ed8794e9c",
+    "url": "/todo/static/js/main.5d3cc86b.chunk.js"
+  },
+  {
+    "revision": "3ce2d46cb5510c372f4d",
+    "url": "/todo/static/js/runtime~main.7a2dc5ec.js"
+  }
+]);
